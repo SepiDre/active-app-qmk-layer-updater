ca8a87faccebfb252171b61333d7601bec5a7deb
Stephan <github@athome.zone>
2022-03-15 10:06:46 +0100

    RunningButNotWorking

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* eslint prefer-destructuring: ["error", {VariableDeclarator: {array: true}}] */
-const { exec } = require('child_process');
 const hid = require('node-hid');
 const fs = require('fs');
 const { parser } = require('json-based-conditions-and-rules-logic-evaluator');
+const activeWindow = require('active-win');
 
 const ARGS = process.argv.slice(2);
 const path = ARGS[0] || './config.json';
@@ -16,17 +16,18 @@ if (!fs.existsSync(path)) {
 
 const configfile = fs.readFileSync(path);
 const config = JSON.parse(configfile);
-
 const {
   PRODUCT,
+  USAGEPAGE,
+  USAGE,
   TIMERS,
   DEFAULT,
   CONDITIONS,
-  RULES,
+  RULES
 } = config;
 
 const isTargetDevice = function (d) {
-  return d.product === PRODUCT && d.usagePage === 0xFF60 && d.usage === 0x61;
+  return d.product === PRODUCT && d.usagePage === USAGEPAGE && d.usage === USAGE;
 };
 
 function connect() {
@@ -35,9 +36,12 @@ function connect() {
   let was = DEFAULT;
   let wasApp = '';
   let wasTitle = '';
+  let wasPlattform = '';
   let output;
   let appData;
+  let plattform;
   let titleData;
+  let titlePath;
 
   if (deviceInfo) {
     device = new hid.HID(deviceInfo.path);
@@ -56,15 +60,15 @@ function connect() {
 
       setTimeout(() => {
         timerID = setInterval(() => {
-          exec('npx active-win', (e, stdout) => {
-            if (e instanceof Error) {
-              console.error('error:', e);
-            }
-            if (stdout) {
-              const arr = stdout.toString().trim().toLowerCase().split('\n');
-
-              appData = arr[2];
-              titleData = arr[0];
+          (async () => {
+            var activeWin = await activeWindow();
+            if (activeWin) {
+              const arr = JSON.stringify(activeWin);
+              console.log(arr);
+              appData = activeWin.owner.name.trim().toLowerCase();
+              titleData = activeWin.title.trim().toLowerCase();
+              plattform = activeWin.platform.trim().toLowerCase();
+              titlePath = activeWin.owner.path.trim().toLowerCase();
 
               if (wasApp !== appData || wasTitle !== titleData) {
                 output = parser({
@@ -74,17 +78,22 @@ function connect() {
                 }, {
                   app: appData,
                   title: titleData,
+                  path: titlePath,
                 });
               }
-
+              // Layer/Platform/Title - Currently not Working, I would like to solve it in QMK
+              const result = output;//+'\/'+plattform+'\/'+activeWin.owner.name;
+              
               if (output && was !== output) {
+                console.log(output);
                 device.write([output]);
                 was = output;
                 wasApp = appData;
                 wasTitle = titleData;
+                wasPlattform = plattform;
               }
             }
-          });
+          })();
         }, TIMERS.RUNNER);
       }, TIMERS.LINK);
     }
